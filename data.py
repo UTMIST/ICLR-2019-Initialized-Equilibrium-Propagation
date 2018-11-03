262f4b3ce152f530ec9f883a0d81d6cdbc743657
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-11-03 16:21:52 -0400

    add train/val split

--- data.py
+++ data.py
@@ -1,13 +1,18 @@
 """
 For loading dataset (MNIST).
 """
+import math
 import os
 
 import mnist
+import numpy as np
 from PIL import Image
 
 
-def load_data():
+# TODO: SEED
+
+
+def load_data(train_ratio):
     """
     Load data.
     """
@@ -16,10 +21,24 @@ def load_data():
         os.makedirs(path)
     mnist.temporary_dir = lambda: path
 
-    return (mnist.train_images(), mnist.train_labels()), (mnist.test_images(), mnist.test_labels())
+    train_val = mnist.train_images(), mnist.train_labels()
+    test = mnist.test_images(), mnist.test_labels()
+
+    # shuffle train set
+    size = len(train_val[0])
+    shuffled_indices = [i for i in range(size)]
+    np.random.shuffle(shuffled_indices)
+
+    # calculate train size
+    train_size = math.floor(size * train_ratio)
+
+    train = train_val[0][shuffled_indices[:train_size]], train_val[1][shuffled_indices[:train_size]]
+    val = train_val[0][shuffled_indices[train_size:]], train_val[1][shuffled_indices[train_size:]]
+
+    return train, val, test
 
 
 if __name__ == "__main__":
-    train, test = load_data()
+    train, val, test = load_data(0.7)
     im = Image.fromarray(train[0][0, :, :] * -1 + 256)
     im.show()

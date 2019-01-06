8af2e7af55df2ce4ec627f482a0e06891cd99e24
Jad Ghalayini <jad.ghalayini@hotmail.com>
2019-01-06 18:18:04 -0500

    Began laying out skeleton for equilibrium.py

---
+++ src/equilibrium.py
@@ -0,0 +1,31 @@
+import torch
+
+from typing import List
+
+class EquilibriumNet:
+    """
+    A fully connected equilibrium propagation network
+    """
+    @staticmethod
+    def get_default_device():
+        """
+        Get the default device for the equilibrium network's components
+        """
+        return "cpu"
+
+    def __init__(
+        input_size : int, layer_sizes : List[int], output_size : int, **kwargs):
+        """
+        Initialize an equilibrium propagation network with given input size,
+        output size and fully connected layer sizes on a given device
+
+        >>> new_net = EquilibriumNet(28*28, [500, 500], 10)
+        """
+        self.device = kwargs.get("device")
+        if self.device is None:
+            self.device = torch.device(self.get_default_device())
+
+
+if __name__ == "name":
+    import doctest
+    doctest.testmod()

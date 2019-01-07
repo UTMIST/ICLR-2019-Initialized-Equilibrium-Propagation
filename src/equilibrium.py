8461094090743460c8b0cd9f23fcfdf359a58ee5
Jad Ghalayini <jad.ghalayini@hotmail.com>
2019-01-06 23:07:39 -0500

    Fixed doctests, added minimatching for energy

--- src/equilibrium.py
+++ src/equilibrium.py
@@ -10,7 +10,7 @@ from typing import List
 def rho(v):
     """
     The activation function to be used, here a hard sigmoid
-    >>> EquilibriumNet.rho(torch.tensor([1, 2, -1, -2, 0.5, -0.5]))
+    >>> rho(torch.tensor([1, 2, -1, -2, 0.5, -0.5]))
     tensor([1.0000, 1.0000, 0.0000, 0.0000, 0.5000, 0.0000])
     """
     return torch.clamp(v, 0, 1)
@@ -20,7 +20,7 @@ def rhoprime(v):
     The gradient of the activation function to be used (a hard sigmoid),
     here just a Boolean function denoting whether the value is in the
     clamp range
-    >>> EquilibriumNet.rhoprime(torch.tensor([0.9, 2, -1, -2, 0.5, -0.5]))
+    >>> rhoprime(torch.tensor([0.9, 2, -1, -2, 0.5, -0.5]))
     tensor([1., 0., 0., 0., 1., 0.])
     """
     return (torch.gt(v, 0) & torch.lt(v, 1)).type(torch.Tensor)
@@ -33,6 +33,10 @@ class EquilibriumNet:
     # The shape of the equilibrium propagation network as a fully connected
     # network
     shape : List[int]
+    partial_sums : List[int]
+
+    # The size of minibatches to be fed into the equilibrium propagation network
+    minibatch_size : int
 
     @staticmethod
     def get_default_device():
@@ -41,17 +45,40 @@ class EquilibriumNet:
         """
         return None
 
+    def set_batch_size(self, minibatch_size : int):
+        """
+        Reinitialize the state with the given minibatch size
+        """
+        self.minibatch_size = minibatch_size
+
+        self.minibatch_size = minibatch_size
+
+        # Initialize the state particles for equilibrium propagation
+        self.state_particles = torch.zeros(self.partial_sums[-1], minibatch_size)
+        # State particles for the neurons in each individual layer,
+        # implemented as views of the memory in state_particles
+        self.layer_state_particles = [
+            self.state_particles[b:e] for (b, e) in
+                zip(self.partial_sums[:-1], self.partial_sums[1:])
+        ]
+
+
     def __init__(self,
-        input_size : int, layer_sizes : List[int], output_size : int, **kwargs):
+        input_size : int, layer_sizes : List[int], output_size : int,
+        minibatch_size : int, **kwargs):
         """
         Initialize an equilibrium propagation network with given input size,
         output size and fully connected layer sizes on a given device
 
-        >>> new_net = EquilibriumNet(28*28, [500, 500], 10)
+        >>> new_net = EquilibriumNet(28*28, [500, 500], 10, 32)
         >>> new_net.shape
         [784, 500, 500, 10]
-        >>> len(new_net.state_particles), len(new_net.weights), len(new_net.biases)
-        (3, 3, 3)
+        >>> len(new_net.state_particles), len(new_net.biases)
+        (1010, 1010)
+        >>> len(new_net.weights), len(new_net.layer_biases)
+        (3, 3)
+        >>> len(new_net.input_weights)
+        1784
         """
         self.device = kwargs.get("device")
         if self.device is None:
@@ -64,40 +91,40 @@ class EquilibriumNet:
 
         # Get the number of particles and bias parameters before each layer, and
         # one past-the-end
-        partial_sums = [0]
-        partial_sums.extend(itertools.accumulate(self.shape[1:], operator.add))
+        self.partial_sums = [0]
+        self.partial_sums.extend(
+            itertools.accumulate(self.shape[1:], operator.add))
 
-        # Initialize the state particles for equilibrium propagation
-        self.state_particles = torch.zeros(partial_sums[-1])
-        # State particles for the neurons in each individual layer,
-        # implemented as views of the memory in state_particles
-        self.layer_state_particles = [
-            self.state_particle[b:e] for (b, e) in
-                zip(partial_sums[:-1], partial_sums[1:])
-        ]
+        self.set_batch_size(minibatch_size)
 
         # Initialize the weights
         self.weights = [
             torch.randn(D_in, D_out, device=self.device) for (D_in, D_out) in
                 zip(self.shape[:-1], self.shape[1:])
         ]
+        # Get a vector of input neurons weights for each state neuron
+        self.input_weights = list(
+            itertools.chain.from_iterable(
+                [[w[p] for p in range(len(w))] for w in self.weights]
+            )
+        )
 
         # Initialize the bias
-        self.biases = torch.randn(partial_sums[-1])
+        self.biases = torch.randn(self.partial_sums[-1])
         # Bias for the neurons in each individual layer, implemented as views of
         # the memory in biases
         self.layer_biases = [
             self.biases[b:e] for (b, e) in
-                zip(partial_sums[:-1], partial_sums[1:])
+                zip(self.partial_sums[:-1], self.partial_sums[1:])
         ]
 
     def energy(self, x):
         """
         The "potential energy" of the equilibrium propagation network for in its
-        current state for each input x, where x is a tensor of shape
-        (batch_size, input_size)
+        current state for each input in x, where x is a tensor of shape
+        (minibatch_size, input_size)
         """
-        assert len(x.shape) == 2 and x.shape[0] == self.shape[0]
+        assert x.shape == (self.minibatch_size, self.input_size)
 
         # Squared norm of the state
         # LaTeX: \frac{1}{2}\sum_{i \in \mathcal{S}}s_i^2

35384fb3e6c963498eb5edeb22a753a07686fd5a
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-01-07 12:44:40 -0500

    add grad_weight checking, running tests from main to allow for debugging

--- src/equilibrium_tests.py
+++ src/equilibrium_tests.py
@@ -46,6 +46,7 @@ def test_energy():
 
     assert expected_energy == actual_energy
 
+
 def test_energy_grad_state():
     """
     Check energy_grad_state is correct using finite differences.
@@ -60,7 +61,7 @@ def test_energy_grad_state():
     l3 = torch.tensor([5, 3])
     l4 = torch.tensor([4, 5])
 
-    network = EquilibriumNet(28 * 28, [3, 2], 2, 1)
+    network = EquilibriumNet(3, [3, 2], 2, 2)
     network.weights = [w1, w2, w3]
     network.biases = b
     network.state_particles = torch.cat([l2, l3, l4])
@@ -83,8 +84,53 @@ def test_energy_grad_state():
         assert relative_error(grad_check, true_grad) < 10e-6
 
 
+def test_energy_grad_weight():
+    """
+    Check energy_grad_weight using finite differences.
+    """
+    x = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    w1 = torch.tensor([[3, 9, 5], [1, 2, 7], [3, 3, 2]])
+    w2 = torch.tensor([[3, 8, 8], [9, 5, 1]])
+    w3 = torch.tensor([[9, 2], [2, 0]])
+    b = torch.tensor([1, 2, 3, 4, 5, 6, 7])  # for l2, l3, l4
+
+    l2 = torch.tensor([9, 4, 6])
+    l3 = torch.tensor([5, 3])
+    l4 = torch.tensor([4, 5])
+
+    network = EquilibriumNet(3, [3, 2], 2, 2)
+    network.weights = [w1, w2, w3]
+    network.biases = b
+    network.state_particles = torch.cat([l2, l3, l4])
+
+    # perform gradient checking with finite differences
+    dh = 10e-5
+    for (layer, D_in, D_out) in zip(range(1, len(network.shape)), network.shape[:-1], network.shape[1:]):
+        for i in range(D_in):
+            for j in range(D_out):
+                # perturb one entry in state
+                network.weights[layer][i][j] += dh
+                f_plus = network.energy(x)
+                network.weights[layer][i][j] -= 2 * dh
+                f_minus = network.energy(x)
+                network.weights[layer][i][j] += dh
+
+                # grad estimate with finite differences
+                grad_check = (f_plus - f_minus) / (2 * dh)
+
+                true_grad = network.energy_grad_weight(x)  # TODO: implement
+
+                assert relative_error(grad_check, true_grad) < 10e-6
+
+
 def relative_error(a, b):
     """
     Compute relative error between a and b.
     """
     return abs(a - b) / (abs(a) + abs(b))
+
+
+if __name__ == "__main__":
+    test_energy()
+    test_energy_grad_state()
+    test_energy_grad_weight()

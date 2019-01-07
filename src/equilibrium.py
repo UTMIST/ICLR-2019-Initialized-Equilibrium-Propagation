47caa9557b91464480e191e543a579c35ca584d4
Jad Ghalayini <jad.ghalayini@hotmail.com>
2019-01-06 20:54:27 -0500

    Added energy function

--- src/equilibrium.py
+++ src/equilibrium.py
@@ -2,12 +2,32 @@
 Defines an equilibrium propagation network model
 """
 import torch
+import itertools
+import operator
 
 from typing import List
 
+def rho(v):
+    """
+    The activation function to be used, here a hard sigmoid
+    >>> EquilibriumNet.rho(torch.tensor([1, 2, -1, -2, 0.5, -0.5]))
+    tensor([1.0000, 1.0000, 0.0000, 0.0000, 0.5000, 0.0000])
+    """
+    return torch.clamp(v, 0, 1)
+
+def rhoprime(v):
+    """
+    The gradient of the activation function to be used (a hard sigmoid),
+    here just a Boolean function denoting whether the value is in the
+    clamp range
+    >>> EquilibriumNet.rhoprime(torch.tensor([0.9, 2, -1, -2, 0.5, -0.5]))
+    tensor([1., 0., 0., 0., 1., 0.])
+    """
+    return (torch.gt(v, 0) & torch.lt(v, 1)).type(torch.Tensor)
+
 class EquilibriumNet:
     """
-    A fully connected equilibrium propagation network
+    A fully connected feed-forward equilibrium propagation network
     """
 
     # The shape of the equilibrium propagation network as a fully connected
@@ -42,9 +62,18 @@ class EquilibriumNet:
         self.shape.extend(layer_sizes)
         self.shape.append(output_size)
 
+        # Get the number of particles and bias parameters before each layer, and
+        # one past-the-end
+        partial_sums = [0]
+        partial_sums.extend(itertools.accumulate(self.shape[1:], operator.add))
+
         # Initialize the state particles for equilibrium propagation
-        self.state_particles = [
-            torch.zeros(D) for D in self.shape[1:]
+        self.state_particles = torch.zeros(partial_sums[-1])
+        # State particles for the neurons in each individual layer,
+        # implemented as views of the memory in state_particles
+        self.layer_state_particles = [
+            self.state_particle[b:e] for (b, e) in
+                zip(partial_sums[:-1], partial_sums[1:])
         ]
 
         # Initialize the weights
@@ -54,29 +83,67 @@ class EquilibriumNet:
         ]
 
         # Initialize the bias
-        self.biases = [
-            torch.randn(D, device=self.device) for D in self.shape[:-1]
+        self.biases = torch.randn(partial_sums[-1])
+        # Bias for the neurons in each individual layer, implemented as views of
+        # the memory in biases
+        self.layer_biases = [
+            self.biases[b:e] for (b, e) in
+                zip(partial_sums[:-1], partial_sums[1:])
         ]
 
-    @staticmethod
-    def rho(v):
+    def energy(self, x):
         """
-        The activation function to be used, here a hard sigmoid
-        >>> EquilibriumNet.rho(torch.tensor([1, 2, -1, -2, 0.5, -0.5]))
-        tensor([1.0000, 1.0000, 0.0000, 0.0000, 0.5000, 0.0000])
+        The "potential energy" of the equilibrium propagation network for in its
+        current state for each input x, where x is a tensor of shape
+        (batch_size, input_size)
         """
-        return torch.clamp(v, 0, 1)
+        assert len(x.shape) == 2 and x.shape[0] == self.shape[0]
 
-    @staticmethod
-    def rhoprime(v):
-        """
-        The gradient of the activation function to be used (a hard sigmoid),
-        here just a Boolean function denoting whether the value is in the
-        clamp range
-        >>> EquilibriumNet.rhoprime(torch.tensor([0.9, 2, -1, -2, 0.5, -0.5]))
-        tensor([1., 0., 0., 0., 1., 0.])
-        """
-        return (torch.gt(v, 0) & torch.lt(v, 1)).type(torch.Tensor)
+        # Squared norm of the state
+        # LaTeX: \frac{1}{2}\sum_{i \in \mathcal{S}}s_i^2
+        squared_norm = torch.sum(self.state_particles ** 2)
+
+
+        # Product of bias and state activation
+        # LaTeX: \sum_{i \in \mathcal{S}}b_i\rho(s_i)
+        bias_sum = torch.sum(
+            self.biases * rho(self.state_particles))
+
+        # Tensor product of weight matrix, activation of non-state neurons j and
+        # activation of non-state neurons i connected to j
+        #
+        # Due to the structure of our network (feed-forward), neurons in layers
+        # after the first, potentially including the output (last) layer, are
+        # connected to state neurons in the previous layer, giving the form of
+        # our calculation
+
+
+        # Matrix product of the weight matrix for a layer and the activation of
+        # neurons in that layer.
+        next_weights = [
+            torch.mv(W, rho(s_out)) for W, s_out in
+                zip(self.weights[1:].T, self.layer_state_particles[1:])
+        ]
+
+        # Dot product of said matrix products and the activations of the vectors
+        # connected to j, summed over all layers
+        tensor_product = sum(
+            [torch.dot(pr, rho(s_in)) for pr, s_in in
+                zip(next_weights, self.layer_state_particles[:-1])]
+        )
+
+        # Tensor product of weight matrix, activation of non-state neurons j and
+        # activation of input neurons i connected to j for each input value in x
+        #
+        # Due to the structure of our network, only neurons in the layer after
+        # the first are connected to the input neurons, and hence we need only
+        # consider these
+        input_sums = -torch.mv(
+            x, torch.mv(self.weights[0].T, self.layer_state_particles[0])
+        )
+
+        # Now, we compute the energy for each element of x
+        return input_sums.add(squared_norm - bias_sum - next_weights)
 
 
 if __name__ == "__main__":

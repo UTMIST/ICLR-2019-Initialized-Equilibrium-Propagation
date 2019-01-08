fixed energy function computation

src/equilibrium.py
@@ -163,8 +163,7 @@ class EquilibriumNet:
 
         # Product of bias and state activation
         # LaTeX: \sum_{i \in \mathcal{S}}b_i\rho(s_i)
-        bias_sum = torch.sum(
-            torch.matmul(self.biases, rho(self.state_particles)))
+        bias_sum = torch.matmul(self.biases, rho(self.state_particles))
 
         # Tensor product of weight matrix, activation of non-state neurons j and
         # activation of non-state neurons i connected to j
@@ -196,7 +195,7 @@ class EquilibriumNet:
         # the first are connected to the input neurons, and hence we need only
         # consider these
         input_sums = -torch.mm(
-            x, torch.mm(torch.t(self.weights[0]), self.layer_state_particles[0])
+            x, torch.mm(torch.t(self.weights[0]), rho(self.layer_state_particles[0]))
         ).diag()
 
         # Now, we compute the energy for each element of x
@@ -229,6 +228,7 @@ class EquilibriumNet:
         ]
         assert len(weight_grad) == len(self.weights)
         for (layer_weight_grad, layer_weight) in zip(self.weights, weight_grad):
+            print(layer_weight_grad.shape, layer_weight.shape)
             assert layer_weight_grad.shape == layer_weight.shape
 
         return weight_grad, bias_grad

src/equilibrium_tests.py
@@ -17,8 +17,7 @@ def test_energy():
     l3 = torch.tensor([5., 3.])
     l4 = torch.tensor([4., 5.])
 
-    squared_norm = (torch.sum(l2**2) +
-                    torch.sum(l3**2) + torch.sum(l4**2)) / 2
+    squared_norm = (torch.sum(l2**2) + torch.sum(l3**2) + torch.sum(l4**2)) / 2
 
     first = torch.cat((l2, l3), 0)
     second = torch.cat((first, l4), 0)
@@ -28,7 +27,7 @@ def test_energy():
     prod2 = torch.dot(torch.mv(torch.t(w3), rho(l4)), rho(l3))
     tensor_product = prod1 + prod2
 
-    input_sums = -torch.mv(x, torch.mv(w1, rho(l2)))
+    input_sums = -torch.mv(x, torch.mv(torch.t(w1), rho(l2)))
 
     expected_energy = input_sums.add(squared_norm - bias_sum - tensor_product)
 
@@ -83,8 +82,9 @@ def test_energy_grad_weight():
 
     # perform gradient checking with finite differences
     dh = 10e-5
-    weight_grad, bias_grad = network.energy_grad_weight(network.state_particles, x)  # TODO: implement
+    weight_grad, bias_grad = network.energy_grad_weight(network.state_particles, x)
 
+    # check weight_grad
     for (layer, D_in, D_out) in zip(range(1, len(network.shape)), network.shape[:-1], network.shape[1:]):
         for i in range(D_in):
             for j in range(D_out):
@@ -101,6 +101,9 @@ def test_energy_grad_weight():
                 print(grad_check, weight_grad[layer][i][j])
                 assert relative_error(grad_check, weight_grad[layer][i][j]) < 10e-6
 
+    # check bias grad
+    # TODO:
+
 
 def relative_error(a, b):
     """
@@ -110,6 +113,6 @@ def relative_error(a, b):
 
 
 if __name__ == "__main__":
-    # test_energy()
+    test_energy()
     # test_energy_grad_state()
-    test_energy_grad_weight()
+    # test_energy_grad_weight()
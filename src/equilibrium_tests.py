031ff078e35db8184ba47ba307f9fa8b53244f37
Jad Ghalayini <jad.ghalayini@hotmail.com>
2019-01-07 19:20:39 -0500

    Removed transposes

--- src/equilibrium_tests.py
+++ src/equilibrium_tests.py
@@ -9,7 +9,7 @@ import torch
 def test_energy():
     x = torch.tensor([[1., 2., 3.], [4., 5., 6.]])
     w1 = torch.tensor([[3., 9., 5.], [1., 2., 7.], [3., 3., 2.]])
-    w2 = torch.tensor([[3., 8., 8.], [9., 5., 1.]])
+    w2 = torch.tensor([[3., 8.], [9., 5.], [3., 4.]])
     w3 = torch.tensor([[9., 2.], [2., 0.]])
     b = torch.tensor([1., 2., 3., 4., 5., 6., 7.])  # for l2, l3, l4
 
@@ -24,22 +24,18 @@ def test_energy():
     second = torch.cat((first, l4), 0)
     bias_sum = torch.sum(b * rho(second))
 
-    w2T = torch.transpose(w2, 0, 1)
-    w3T = torch.transpose(w3, 0, 1)
-    w1T = torch.transpose(w1, 0, 1)
-
-    prod1 = torch.dot(torch.mv(w2T, rho(l3)), rho(l2))
-    prod2 = torch.dot(torch.mv(w3T, rho(l4)), rho(l3))
+    prod1 = torch.dot(torch.mv(w2, rho(l3)), rho(l2))
+    prod2 = torch.dot(torch.mv(w3, rho(l4)), rho(l3))
     tensor_product = prod1 + prod2
 
-    input_sums = -torch.mv(x, torch.mv(w1T, rho(l2)))
+    input_sums = -torch.mv(x, torch.mv(w1, rho(l2)))
 
     expected_energy = input_sums.add(squared_norm - bias_sum - tensor_product)
 
     net = EquilibriumNet(
         3, [3, 2], 2, 2,
         biases = torch.tensor([float(x) for x in b]),
-        weights = [w1T, w2T, w3T],
+        weights = [w1, w2, w3],
         initial_state = torch.cat([l2, l3, l4])
         )
 
